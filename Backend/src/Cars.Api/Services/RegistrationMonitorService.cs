440447294d5bdb7cfa1b38b1055d2bce6afb7d40
Craig <craigbaldry@outlook.com.au>
2025-12-02 14:44:26 +0800

    update background service to check status of cars

--- Backend/src/Cars.Api/Services/RegistrationMonitorService.cs
+++ Backend/src/Cars.Api/Services/RegistrationMonitorService.cs
@@ -1,5 +1,6 @@
 ﻿
 using Cars.Api.Hubs;
+using Cars.Api.Model.Entities;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Cars.Api.Services
@@ -8,18 +9,47 @@ namespace Cars.Api.Services
     {
         private readonly ICarService _carService;
         private readonly IHubContext<RegistrationHub, IRegistrationHub> _hubContext;
-        
+        private readonly ILogger<RegistrationMonitorService> _logger;
+
         public RegistrationMonitorService(
             ICarService carService,
-            IHubContext<RegistrationHub, IRegistrationHub> hubContext)
+            IHubContext<RegistrationHub,IRegistrationHub> hubContext,
+            ILogger<RegistrationMonitorService> logger
+            )
         {
             _carService = carService;
             _hubContext = hubContext;
+            _logger = logger;
         }
 
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            throw new NotImplementedException();
+            _logger.LogInformation("Registration monitor service started");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var cars = _carService.GetAll();
+                var today = DateOnly.FromDateTime(DateTime.UtcNow);
+
+                foreach (var car in cars)
+                {
+                    var carExpiryDate = DateOnly.FromDateTime(car.RegistrationExpiry);
+                    var isExpired = carExpiryDate < today;
+
+                    var update = new Registration
+                    {
+                        CarId = car.Id,
+                        RegistrationNumber = car.RegistrationNumber,
+                        IsExpired = isExpired
+                    };
+
+                    await _hubContext.Clients.All.RegistrationUpdated(update);
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+            }
+
+            _logger.LogInformation("Registration monitor stopping");
         }
     }
 }
